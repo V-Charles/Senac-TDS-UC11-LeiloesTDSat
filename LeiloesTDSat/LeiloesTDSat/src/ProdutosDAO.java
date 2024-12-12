3bbc45cde23f85b99a9688c8276923e3c7d6b0f8
Vinicius Charles <viniciuscharles3101@gmail.com>
2024-12-12 18:07:32 -0300

    Implementado método cadastroProduto e corrigido problema de conexão desativando SSL no MySQL

--- LeiloesTDSat/LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/LeiloesTDSat/src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -22,12 +23,25 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
+    public ProdutosDAO(){
+        conectaDAO conecta = new conectaDAO();
+        conn = conecta.connectDB();
+    }
+    
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        try{
+            prep = conn.prepareStatement("INSERT INTO produtos VALUES (?, ?, ?, ?)");
+            prep.setString(1, null);
+            prep.setString(2, produto.getNome());
+            prep.setInt(3, produto.getValor());
+            prep.setString(4, produto.getStatus());
+            
+            prep.executeUpdate();
+            
+        } catch(SQLException ex) {
+            System.out.println("Erro no método: cadastrarProduto");
+            System.out.println(ex.getMessage());
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
--- LeiloesTDSat/LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/LeiloesTDSat/src/conectaDAO.java
@@ -22,7 +22,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=Vinidias8343");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=Vinidias8343&useSSL=false&allowPublicKeyRetrieval=true");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
